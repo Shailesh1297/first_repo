a73bc4cdfde32b7df2f0e323ff0d7883b07e083c
Shailesh1297 <38895259+Shailesh1297@users.noreply.github.com>
2019-02-20 18:50:59 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+void main()
+
+{
+    int x,i,sum=0;
+    
+scanf("%d",&x);
+   
+ int arr[x];
+    
+for(i=0;i<x;i++)
+   
+ {
+      
+  scanf("%d",&arr[i]);
+      
+  sum=sum+arr[i];
+      
+  }
+       
+ printf("%d",sum);
+   
+ }
